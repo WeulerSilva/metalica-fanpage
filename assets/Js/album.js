0fcc0759bf474df454cd654cd2681b090034b964
WeulerSilva <weulergamer@outlook.com>
2023-10-24 22:31:59 -0300

    Initial commit

---
+++ assets/Js/album.js
@@ -0,0 +1,267 @@
+const album = {
+  seasons: {
+    name: 'album',
+    title: '72 SEASONS',
+    release: 'RELEASED APRIL 14, 2023',
+    lancamento: 'LANÇADO 14 ABRIL, 2023',
+    img: '72seasons',
+    bass: 'ROBERT TRUJILLO',
+    songs: ['72 SEASONS', 'SHADOWS FOLLOW', 'SCREAMING SUICIDE', 'SLEEPWALK MY LIFE AWAY', 'YOU MUST BURN!', 'LUX AETERNA', 'CROWN OF BARBED WIRE', 'CHASING LIGHT', 'IF DARKNESS HAD A SON', 'TOO FAR GONE?', 'ROOM OF MIRRORS', 'INAMORATA'],
+    link: 'https://metallica.lnk.to/72Seasons',
+    info: "“72 seasons. The first 18 years of our lives that form our true or false selves. The concept that we were told ‘who we are’ by our parents. A possible pigeonholing around what kind of personality we are. I think the most interesting part of this is the continued study of those core beliefs and how it affects our perception of the world today. Much of our adult experience is reenactment or reaction to these childhood experiences. Prisoners of childhood or breaking free of those bondages we carry.” – James Hetfield",
+    infoBr: "“72 temporadas. Os primeiros 18 anos de nossas vidas que formam nosso verdadeiro ou falso eu. O conceito de que nossos pais nos disseram 'quem somos'. Uma possível classificação em torno do tipo de personalidade que somos. Acho que a parte mais interessante disto é o estudo contínuo dessas crenças fundamentais e como isso afeta a nossa percepção do mundo hoje. Grande parte da nossa experiência adulta é uma reconstituição ou reação a essas experiências infantis. Prisioneiros da infância ou libertando-nos das amarras que carregamos.” - James Hetfield"
+  },
+  sem2: {
+    name: 'album',
+    title: 'S&M2',
+    release: 'RELEASED AUGUST 28, 2020',
+    lancamento: 'LANÇADO 28 AGOSTO, 2020',
+    img: 'sem2',
+    bass: 'ROBERT TRUJILLO',
+    songs: [ 'THE ECSTASY OF GOLD', 'THE CALL OF KTULU', 'FOR WHOM THE BELL TOLLS', 'THE DAY THAT NEVER COMES', 'THE MEMORY REMAINS', 'CONFUSION', 'MOTH INTO FLAME', 'THE OUTLAW TORN', 'NO LEAF CLOVER', 'HALO ON FIRE', 'INTRO TO SCYTHIAN SUITE', 'SCYTHIAN SUITE', 'INTRO TO THE IRON FOUNDRY','THE IRON FOUNDRY,OPUS 19', 'THE UNFORGIVIN III', 'ALL WHITIN MY HANDS', '(ANASTHESIA) - PULLING TEETH', 'WHEREVER I MAY ROAM', 'ONE', 'MASTER OF PUPPETS', 'NOTHING ELSE MATTERS', 'ENTER SANDMAN'],
+    link: 'https://metallica.lnk.to/S-M2',
+    info: ''
+  },
+  hardwired: {
+    name: 'album',
+    title: 'HARDWIRED...TO SELF-DESTRUCT',
+    release: 'RELEASED NOVEMBER 18, 2016',
+    lancamento: 'LANÇADO 18 NOVEMBRO, 2016',
+    img: 'hardwired',
+    bass: 'ROBERT TRUJILLO',
+    songs:  ['HARDWIRED','ATLAS,RISE!','NOW THAT WE´RE DEAD','MOTH INTO FLAME','DREAM NO MORE','HALO ON FIRE','CONFUSION','MANUNKIND','HERE COMES REVENGE','AM I SAVAGE?','MURDER ONE','SPIT OUT THE BONE','BONUS: LORDS OF SUMMER'],
+    link:'https://metallica.lnk.to/HardwiredToSelf-Destruct',
+    info:''
+  },
+  through: {
+    name: 'album',
+    title: 'METALLICA THROUGH THE NEVER',
+    release: 'RELEASED SEPTEMBER 24, 2013',
+    lancamento: 'LANÇADO 24 SETEMBRO, 2013',
+    img: 'through',
+    bass: 'ROBERT TRUJILLO',
+    songs: ['THE ECSTASY OF GOLD','CREEPING DEATH','FOR WHOM THE BELL TOLLS','FUEL','RIDE THE LIGHTNING','ONE','THE MEMORY REMAINS','WHEREVER I MAY ROAM','CYANIDE','...AND JUSTICE FOR ALL','MASTER OF PUPPETS','BATTERY','NOTHING ELSE MATTERS','ENTER SANDMAN','HIT THE LIGHTS','ORION'],
+    link: 'https://metallica.lnk.to/ThroughTheNeverMusic',
+    info: ''
+  },
+  death: {
+    name: 'album',
+    title: 'DEATH MAGNETIC',
+    release: 'RELEASED SEPTEMBER 12, 2008',
+    lancamento: 'LANÇADO 12 SETEMBRO, 2008',
+    img: 'death',
+    bass: 'ROBERT TRUJILLO',
+    songs: ['THAT WAS JUST YOUR LIFE','THE END OF THE LINE','BROKEN,BEAT & SCARRED','THE DAY THAT NEVER COMES','ALL NIGHTMARE LONG','CYANIDE','THE UNFORGIVEN III','THE JUDAS KISS','SUICIDE & REDEMPTION','MY APOCALYPSE'],
+    link: 'https://metallica.lnk.to/DeathMagnetic',
+    info:  ''
+  },
+  anger: {
+    name: 'album',
+    title: 'ST.ANGER',
+    release: 'RELEASED JUNE 5, 2003',
+    lancamento: 'LANÇADO 5 JUNHO, 2003',
+    img: 'anger',
+    bass: 'BOB ROCK',
+    songs: ['FRANTIC','ST.ANGER','SOME KIND OF MONSTER','DIRTY WINDOW','INVISIBLE KID','MY WORLD','SHOOT ME AGAIN','SWEET AMBER','THE UNNAMED FEELING','PURIFY','ALL WITHON MY HANDS'],
+    link: 'https://metallica.lnk.to/StAnger',
+    info: ''
+  },
+  sem: {
+    name: 'album',
+    title: 'S&M',
+    release: 'RELEASED NOVEMBER 23, 1999',
+    lancamento: 'LANÇADO 23 NOVEMBRO, 1999',
+    img: 'sem',
+    bass: 'JASON NEWSTED',
+    songs: ['THE ECSTASY OF GOLD','THE CALL OF KTULU','MASTER OF PUPPETS','OF WOLF AND MAN','THE THING THAT SHOULD NOT BE','FUEL','THE MEMORY REMAINS','NO LEAF CLOVER','HERO OF THE DAY','DEVIL´S DANCE','BLEEDING ME','NOTHING ELSE MATTERS','UNTIL IT SLEEPS','FOR WHOM THE BELL TOLLS','-HUMAN','WHEREVER I MAY ROAM','THE OUTLAW TORN','SAD BUT TRUE','ONE','ENTER SANDMAN','BATTERY'],
+    link: 'https://metallica.lnk.to/S-M',
+    info: ''
+  },
+  garage: {
+    name: 'album',
+    title: 'GARAGE INC.',
+    release: 'RELEASED NOVEMBER 24, 1998',
+    lancamento: 'LANÇADO 24 NOVEMBRO, 1998',
+    img: 'garage',
+    bass: 'JASON NEWSTED',
+    songs: ['FREE SPEECH FOR THE DUMB','IT´S ELECTRIC','SABBRA CADABRA','TURN THE PAGE','DIE,DIE MY DARLING','LOVERMAN','MERCYFUL FATE','ASTRONOMY','WHISKEY IN THE JAR','TUESDAY´S GONE','THE MORE I SEE'],
+    link: 'https://metallica.lnk.to/GarageInc',
+    info: ''
+  },
+  reload: {
+    name: 'album',
+    title: 'RELOAD',
+    release: 'RELEASED NOVEMBER 18, 1997',
+    lancamento: 'LANÇADO 18 NOVEMBRO, 1997',
+    img: 'reload',
+    bass: 'JASON NEWSTED',
+    songs: ['FUEL','THE MEMORY REMAINS','DEVIL`S DANCE','THE UNFORGIVEN II','BETTER THAN YOU','SLIPTHER','CARPE DIEM BABY','BAD SEED','WHERE THE WILD THINGS ARE','PRINCE CHARMING','LOW MAN`S LYRIC','ATTITUDE','FIXXXER'],
+    link: 'https://metallica.lnk.to/Reload',
+    info: ''
+  },
+  load: {
+    name: 'album',
+    title: 'LOAD',
+    release: 'RELEASED JUNE 4, 1996',
+    lancamento: 'LANÇADO 4 JUNHO, 1996',
+    img: 'load',
+    bass: 'JASON NEWSTED',
+    songs: ['AIN`T MY BITCH','2 X 4','THE HOUSE JACK BUILT','UNTIL IT SLEEPS','KING NOTHING','HERO OF THE DAY','BLEEDING ME','CURE','POOR TWISTED ME','WASTING MY HATE','MAMA SAID','THORN WITHIN','RONNIE','THE OUTLAW TORN'],
+    link: 'https://metallica.lnk.to/LoadAlbum',
+    info: ''
+  },
+  metallica: {
+    name: 'album',
+    title: 'METALLICA',
+    release: 'RELEASED AUGUST 12, 1991',
+    lancamento: 'LANÇADO 12 AGOSTO, 1991',
+    img: 'black',
+    bass: 'JASON NEWSTED',
+    songs: ['ENTER SANDMAN','SAD BUT TRUE','HOLIER THAN THOU','THE UNFORGIVEN','WHEREVER I MAY ROAM','DON`T TREAT ON ME','THROUGH THE NEVER','NOTHING ELSE MATTERS','OF WOLF AND MAN','THE GOD THAT FAILED','MY FRIEND OF MISERY','THE STRUGGLE WITHIN'],
+    link: 'https://metallica.lnk.to/MetallicaRemastered',
+    info: ''
+  },
+  justice: {
+    name: 'album',
+    title: '...AND JUSTICE FOR ALL',
+    release: 'RELEASED SEPTEMBER 7, 1988',
+    lancamento: 'LANÇADO 7 SETEMBRO, 1988',
+    img: 'justice',
+    bass: 'JASON NEWSTED',
+    songs: ['BLACKENED','...AND JUSTICE FOR ALL','EYE OF THE BEHOLDER','ONE','THE SHORTEST STRAW','HARVESTER OF SORROW','THE FRAYED ENDS OF SANITY','TO LIVE IS TO DIE','DYERS EVE'],
+    link: 'https://metallica.lnk.to/AndJusticeforAllRemastered',
+    info: ''
+  },
+  revisited: {
+    name: 'album',
+    title: 'GARAGE DAYS RE-REVISITED',
+    release: 'RELEASED AUGUST 21, 1987',
+    lancamento: 'LANÇADO 21 AGOSTO, 1987',
+    img: 'revisited',
+    bass: 'JASON NEWSTED',
+    songs: ['HELPLESS','THE SMALL HOURS','THE WAIT','CRASH COURSE IN BRAIN SURGERY','LAST CARESS/GREEN HELL'],
+    link: '',
+    info: ''
+  },
+  master: {
+    name: 'album',
+    title: 'MASTER OF PUPPETS',
+    release: 'RELEASED MARCH 3, 1986',
+    lancamento: 'LANÇADO 3 MARÇO, 1986',
+    img: 'master',
+    bass:'CLIFF BURTON',
+    songs: ['BATTERY','MASTER OF PUPPETS','THE THING THAT SHOULD NOT BE','WELCOME HOME (SANITARIUM)','DISPOSABLE HEROES','LEPER MESSIAH','ORION','DAMAGE, INC.'],
+    link: 'https://metallica.lnk.to/MasterOfPuppetsRemastered',
+    info: ''
+  },
+  ride: {
+    name: 'album',
+    title: 'RIDE THE LIGHTNING',
+    release: 'RELEASED JULY 27, 1984',
+    lancamento: 'LANÇADO 27 JULHO, 1984',
+    img: 'ride',
+    bass:'CLIFF BURTON',
+    songs: ['FIGHT FIRE WITH FIRE','RIDE THE LIGHTNING','FOR WHOM THE BELL TOLLS','FADE TO BLACK','TRAPPED UNDER ICE','ESCAPE','CREEPING DEATH','THE CALL OF KTULU'],
+    link: 'https://metallica.lnk.to/RideTheLightningRemastered',
+    info: ''
+  },
+  kill: {
+    name: 'album',
+    title: "KILL 'EM ALL",
+    release: 'RELEASED JULY 25, 1983',
+    lancamento: 'LANÇADO 25 JULHO, 1983',
+    img: 'kill',
+    bass:'CLIFF BURTON',
+    songs: ['HIT THE LIGHTS','THE FOUR HORSEMEN','MOTORBREATH','JUMP IN THE FIRE','(ANESTHESIA) - PULLING TEETH','WHIPLASH','PHANTOM LORD','NO REMORSE','SEEK & DESTROY','METAL MILITIA'],
+    link: 'https://metallica.lnk.to/KillEmAllRemastered',
+    info: ''
+  },
+}
+
+function createDiv(className) {
+  const div = document.createElement('div');
+  div.classList.add(className);
+  return div;
+}
+
+for (const key in album) {
+  if (album.hasOwnProperty(key)) {
+    const element = album[key];
+    
+    const albumDiv = createDiv('album');
+    
+    const cover = createDiv('cover');
+    albumDiv.appendChild(cover);
+
+    const releaseDiv = createDiv('album-release');
+    if(languageEng) {
+      releaseDiv.textContent = element.release;
+    }else {
+      releaseDiv.textContent = element.lancamento;
+    }
+    
+    albumDiv.appendChild(releaseDiv);
+
+    const titleDiv = createDiv('album-title');
+    titleDiv.textContent = element.title;
+    albumDiv.appendChild(titleDiv);
+
+    cover.style.backgroundImage = `url(/assets/Images/${element.img}.jfif)`
+    document.querySelector('.albums-cover').appendChild(albumDiv);
+
+
+
+
+    albumDiv.addEventListener('click', () => {
+      document.querySelector('.albums-cover').style.display = 'none';
+      document.querySelector('.album-desc').style.display = 'flex';
+      document.querySelector('.album-back').style.display = 'flex';
+
+      document.querySelector('.cover-desc').style.backgroundImage = `url(/assets/Images/${element.img}.jfif)`;
+
+      document.querySelector('.listen-desc').addEventListener('click', () => {
+        window.open(element.link)
+      });
+
+      document.querySelector('.title-desc').innerHTML = element.title;
+      document.querySelector('.bass-desc span').innerHTML = element.bass;
+
+      if(languageEng) {
+        document.querySelector('.album-info').innerHTML = element.info;
+      }else {
+        document.querySelector('.album-info').innerHTML = element.infoBr;
+      }
+
+      const songMusicList = document.querySelectorAll('.song-music');
+      songMusicList.forEach((songDiv) => {
+        songDiv.parentNode.removeChild(songDiv);
+      });
+
+
+      const albumSong = element.songs;
+
+      for(let i = 0; i < albumSong.length; i++) {
+
+        const songDiv = createDiv('song-music');
+        songDiv.textContent = `${i+1}: ${albumSong[i]}`;
+
+        document.querySelector('.music-desc').appendChild(songDiv);
+      }
+
+    })
+  }
+}
+
+
+
+document.querySelector('.album-back').addEventListener('click', () => {
+  document.querySelector('.albums-cover').style.display = 'flex';
+  document.querySelector('.album-desc').style.display = 'none';
+  document.querySelector('.album-back').style.display = 'none';
+  document.querySelectorAll('.song-title').innerHTML = '';
+});
+
+
+
+
+
---
+++ assets/Js/script.js
@@ -0,0 +1,102 @@
+const membersName = document.querySelectorAll('.members-name');
+
+
+
+function membersClick() {
+  membersName.forEach(member => {
+    member.addEventListener('click', () => {
+      membersName.forEach(otherName => {
+        otherName.classList.remove('members-click');
+      });
+
+      dataNameLeft  = member.getAttribute('data-left');
+      dataNameRight  = member.getAttribute('data-right');
+  
+      member.classList.add('members-click');
+  
+      document.querySelector('.member-left').innerHTML = dataNameLeft;
+      document.querySelector('.member-right').innerHTML = dataNameRight;
+      document.querySelector('.member-name').innerHTML = `${dataNameRight} ${dataNameLeft}`;
+    })
+  })
+}
+
+membersClick();
+
+window.addEventListener('scroll', function () {
+  const leftName = document.querySelector('.member-left');
+  const rightName = document.querySelector('.member-right');
+
+  const leftDepth = parseFloat(leftName.getAttribute('data-depth'));
+  const rightDepth = parseFloat(rightName.getAttribute('data-depth'));
+
+  const leftTranslateX = -window.scrollY * leftDepth;
+  const rightTranslateX = window.scrollY * rightDepth;
+
+  leftName.style.transform = `translateX(${leftTranslateX}px)`;
+  rightName.style.transform = `translateX(${rightTranslateX}px)`;
+});
+
+
+
+
+//play music
+const overlay = document.querySelector('.cabecalho img');
+const videoIframe = document.querySelector('.biografy iframe');
+
+overlay.addEventListener('click', function () {
+    videoIframe.src = videoIframe.src + '&autoplay=1';
+    overlay.style.opacity = '0.8';
+    overlay.style.backgroundImage = 'none';
+});
+
+
+
+//language
+
+let languageEng = localStorage.getItem('languageEng') === 'true';
+
+if(languageEng) {
+  document.querySelector('.language-eng').style.backgroundColor = 'yellow';
+}else {
+  document.querySelector('.language-br').style.backgroundColor = 'yellow';
+  document.querySelector('.cab-home').innerHTML = 'Inicio';
+  document.querySelector('.cab-home').title = 'INICIO';
+
+  document.querySelector('.cab-mem').innerHTML = 'Membros';
+  document.querySelector('.cab-mem').title = 'MEMBROS';
+
+  document.querySelector('.cab-bio').innerHTML = 'Biografia';
+  document.querySelector('.cab-bio').title = 'BIOGRAFIA';
+
+  document.querySelector('.member h1').innerHTML = 'Membros';
+
+  document.querySelector('.member-biografy').innerHTML = "James Hetfield é um renomado músico, compositor e vocalista da icônica banda de heavy metal Metallica. Nascido em 3 de agosto de 1963, na Califórnia, EUA, Hetfield é conhecido por seu talento musical excepcional e contribuições significativas para o mundo do rock e do metal. A história de James Hetfield é uma jornada de resiliência, paixão e determinação. Crescendo em uma família tumultuada e lidando com tragédias pessoais, Hetfield encontrou consolo na música desde muito jovem. Ele aprendeu a tocar violão e logo começou a escrever suas próprias músicas. Em 1981, ao lado de seu amigo de infância e baterista Lars Ulrich, Hetfield co-fundou o Metallica, que se tornaria uma das bandas de heavy metal mais influentes e bem-sucedidas de todos os tempos. Sua parceria com Ulrich serviu como a espinha dorsal criativa da banda, com Hetfield assumindo os papéis de vocalista, letrista principal e guitarrista principal. A carreira de James Hetfield com o Metallica foi marcada por uma série de álbuns lendários, incluindo 'Master of Puppets', 'Ride the Lightning' e o autointitulado 'Metallica' (também conhecido como 'The Black Album'). Suas letras frequentemente exploram temas de alienação, raiva e os altos e baixos da vida. Além de suas conquistas com o Metallica, Hetfield também enfrentou desafios pessoais, incluindo sua luta contra o vício e a subsequente jornada para a recuperação. Ele usou suas próprias experiências para ajudar e inspirar outras pessoas que passavam por batalhas semelhantes. James Hetfield é conhecido por sua voz poderosa e distinta, bem como por seu estilo de guitarra característico. Ele influenciou inúmeras gerações de músicos e continua a ser uma figura proeminente no mundo da música até hoje. Sua história é uma prova de como a música pode servir como meio de expressão e cura, e seu legado no Metallica e no cenário musical global é inegável. James Hetfield continua sendo uma das figuras mais icônicas e reverenciadas da história do rock e do metal.";
+
+  document.querySelector('.album-back').innerHTML = 'VOLTAR';
+  document.querySelector('.listen-desc').innerHTML = 'OUVIR';
+  document.querySelector('.base-desc h4').innerHTML = 'GUITARRA, VOCAIS:';
+  document.querySelector('.guitar-desc h4').innerHTML = 'GUITARRA:';
+  document.querySelector('.drums-desc h4').innerHTML = 'BATERIA:';
+  document.querySelector('.bass-desc h4').innerHTML = 'BAIXO:';
+  document.querySelector('.music-title').innerHTML = 'MUSICAS';
+
+  document.querySelector('footer h6').innerHTML = 'Feito por WEULER SILVA minhas redes sociais:';
+};
+
+
+document.querySelector('.language-eng').addEventListener('click', () => {
+  if(languageEng === false) {
+    languageEng = true;
+    localStorage.setItem('languageEng', languageEng);
+    location.reload();
+  }
+});
+
+document.querySelector('.language-br').addEventListener('click', () => {
+  if(languageEng === true) {
+    languageEng = false;
+    localStorage.setItem('languageEng', languageEng);
+    location.reload();
+  }
+})
